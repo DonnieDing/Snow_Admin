1d8489c4f7e94cf6af63a3c2d4b7a61d99091ca9
Dcl_Snow <Dcl_Snow@163.com>
2024-06-28 14:48:49 +0800

    创建boot3分支

--- src/main/java/com/snow/dcl/utils/RedisUtils.java
+++ src/main/java/com/snow/dcl/utils/RedisUtils.java
@@ -1,7 +1,6 @@
 package com.snow.dcl.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
